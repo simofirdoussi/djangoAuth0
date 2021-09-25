ee06593cc35a55c30f89e2b1153b5c9397be8505
mohamed <elfirdoussimed99@gmail.com>
2021-09-25 09:53:02 +0100

    last commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import logout as log_out
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from urllib.parse import urlencode
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'app/index.html')
+
+def logout(request):
+    log_out(request)
+    return_to = urlencode({'returnTo': request.build_absolute_uri('/')})
+    logout_url = 'https://%s/v2/logout?client_id=%s&%s' % \
+                 (settings.SOCIAL_AUTH_AUTH0_DOMAIN, settings.SOCIAL_AUTH_AUTH0_KEY, return_to)
+    return HttpResponseRedirect(logout_url)
